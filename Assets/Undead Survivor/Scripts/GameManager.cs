9b74964d4de8e160d7f4311be02c336ee310ce14
이순교 <107173785+saoinin@users.noreply.github.com>
2024-12-03 17:02:54 +0900

    20241203

--- Assets/Undead Survivor/Scripts/GameManager.cs
+++ Assets/Undead Survivor/Scripts/GameManager.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
+    public float gameTime;
+    public float maxGacmeTime = 2 * 10f;
     public PoolManager pool;
     public Player player;
 
@@ -10,6 +13,16 @@ public class GameManager : MonoBehaviour
     {
         instance = this;
         
+    }
+
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (gameTime > maxGacmeTime)
+        {
+            gameTime = maxGacmeTime;
+        }
     }
 
 }
--- Assets/Undead Survivor/Scripts/Spawner.cs
+++ Assets/Undead Survivor/Scripts/Spawner.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public Transform[] spawnPoint;
+    int level;
+    float timer;
+
+    void Awake()
+    {
+        spawnPoint = GetComponentsInChildren<Transform>();
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        timer += Time.deltaTime;
+        level = Mathf.FloorToInt(GameManager.instance.gameTime / 10f);
+        //내림
+        //CeilToInt 올림
+
+
+        if(timer > (level==0 ? 0.5f : 0.2f))
         {
-            GameManager.instance.pool.Get(1);
+            
+            timer = 0;
+            Spawn();
         }
+    }
+    void Spawn()
+    {
+        GameObject enemy = GameManager.instance.pool.Get(level);//0~1 사이 라는 뜻
+        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
     }
 }
